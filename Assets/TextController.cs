04629b2f5c26385be3df7e50ed9559e932671bf2
Johannes Thorn <johannes.thorn@g4t3.de>
2017-04-14 18:03:13 +0200

    Add first scene descriptions

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -18,7 +18,10 @@ public class TextController : MonoBehaviour
 	void Update ()
 	{
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			text.text = "Space key pressed";
+			text.text = "After a long journey you finally arrive at your new home.\n"
+			+ "It's a prison cell. Hurray!\n"
+			+ "There are some dirty sheets on the bed, a locked door and a mirror on the wall.\n\n"
+			+ "Press S to view Sheets, M to view Mirror and D to view Door."; 
 		}
 	}
 }
